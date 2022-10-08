ffe3004c45222d305769b31bb12979aa29e79964
Marcos Orive <marcos@orive.me>
2022-10-08 10:11:08 +0200

    Migrate settings to mysql

--- backend/README.md
+++ backend/README.md
@@ -1,5 +1,3 @@
 # Luis Mena Backend
 
 This is the CMS for estudioluismena.com. Built with strapi.
-
-Check it out https://estudioluismena.com
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,29 +1,29 @@
-const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  const postgresSettings = {
-    client: "postgres",
-    host,
-    port,
-    database,
-    username: user,
-    password,
-    ssl: { rejectUnauthorized: false }
+  const mysqlSettings = {
+    client: "mysql",
+    host: env("DATABASE_HOST"),
+    database: env("DATABASE_NAME"),
+    username: env("DATABASE_USER"),
+    password: env("DATABASE_PASSWORD"),
+    ssl: { rejectUnauthorized: true }
   }
+
 
   const sqliteSettings = {
     client: 'sqlite',
     filename: '.tmp/data.db'
   }
 
+  const shouldUseSqlite = Boolean(process.env.DATABASE_TYPE_SQLITE === 'true');
+  const dbSettings = shouldUseSqlite ? sqliteSettings : mysqlSettings;
+  console.log(`Using ${dbSettings.client} as database`)
   return {
     defaultConnection: 'default',
     connections: {
       default: {
         connector: 'bookshelf',
-        settings: process.env.DATABASE_TYPE_SQLITE ? sqliteSettings : postgresSettings,
+        settings: dbSettings,
         options: {
           useNullAsDefault: true,
         },
